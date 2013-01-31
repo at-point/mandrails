1d6694d165acab42c1fb5b275a86671556458d9d
lukas <lukas.westermann@gmail.com>
2013-01-31 14:43:01 +0100

    add delivery method *before* action mailer config

--- lib/mandrails/action_mailer.rb
+++
@@ -1,6 +0,0 @@
-require "action_mailer"
-require "mandrails/delivery/mandrill"
-
-ActiveSupport.on_load(:action_mailer) do
-  ActionMailer::Base.add_delivery_method(:mandrails, Mandrails::Delivery::Mandrill)
-end
--- lib/mandrails/railtie.rb
+++ lib/mandrails/railtie.rb
@@ -1,10 +1,11 @@
 require 'rails'
-require 'action_mailer'
 
 module Mandrails
   class Railtie < Rails::Railtie #:nodoc:
-    initializer 'mandrails.setup_action_mailer' do
-      require 'mandrails/action_mailer'
+    initializer 'mandrails.setup_action_mailer', before: 'action_mailer.set_configs' do
+      ActiveSupport.on_load(:action_mailer) do
+        add_delivery_method :mandrails, Mandrails::Delivery::Mandrill
+      end
     end
   end
 end
