d1290dba25f8384eb6874fedc3de7d3884c5f5a3
lukas <lukas.westermann@gmail.com>
2013-01-31 14:44:29 +0100

    name delivery method mandrill

--- lib/mandrails/railtie.rb
+++ lib/mandrails/railtie.rb
@@ -4,7 +4,7 @@ module Mandrails
   class Railtie < Rails::Railtie #:nodoc:
     initializer 'mandrails.setup_action_mailer', before: 'action_mailer.set_configs' do
       ActiveSupport.on_load(:action_mailer) do
-        add_delivery_method :mandrails, Mandrails::Delivery::Mandrill
+        add_delivery_method :mandrill, Mandrails::Delivery::Mandrill
       end
     end
   end
